6353e7085d639374612c1023f91ad391da10d35b
Thomas Frey <thomas.frey@code.berlin>
2018-09-30 12:39:53 +0000

    adapted mario2 problem.

---
+++ fries.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int("How many fries yall want? ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("🍟");
+    }
+
+printf("\n");
+
+}
